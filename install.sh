8e00b43547cdaacd042534ce9ab0ec1bb9a4e910
skygufei <826855211@qq.com>
2017-09-01 09:37:18 +0800

    fix

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 #vim-javascript
-clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
+git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
 
 ln -s ~/.ed-rc/.vimrc ~/.vimrc 
 ln -s  ~/.ed-rc/.tmux.conf ~/.tmux.conf
