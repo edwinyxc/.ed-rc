89a3ae27d1dd95e3d0e6bf1710a810a2afec381c
edwinyxc <edwinyxc@outlook.com>
2020-08-22 10:02:04 +1000

    nice

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
-FROM ubuntu:18.04
+FROM ubuntu:20.04
 LABEL maintainer="Edwin yxc <edwinyxc@outlook.com>"
 COPY ./ /root/.ed-rc
 RUN ls -la /root/.ed-rc
+RUN sed -e s/sudo//g -i /root/.ed-rc/install.sh
 RUN /bin/bash /root/.ed-rc/install.sh
--- install.sh
+++ install.sh
@@ -1,42 +1,58 @@
 #!/bin/bash
 # Basics Tools (TODO remove deps from ubuntu /susu /apt)
-# vim 8.1 for coc.nvim
-sudo add-apt-repository ppa:jonathonf/vim
-sudo apt-get update 
-sudo apt-get install -y zip unzip curl wget tmux vim git \
-                        silversearcher-ag global ctags \
-                        python3 pip-python3
+# vim 8.2 for coc.nvim && popup-window
+# 1. build-essential && tools
+sudo apt-get update
+sudo apt-get install -y build-essential ncurses-dev \
+	zip unzip curl wget tmux git \
+	silversearcher-ag ctags fd-find \
+	bash-completion
 
-sudo pip install pygments
+# Oh my Bash
+echo exit | bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" 
+
+# build latest vim
+mkdir ~/.buildrepos
+cd ~/.buildrepos/
+git clone https://github.com/vim/vim.git
+cd vim
+./configure
+make
+sudo make install
+cd ~
+echo 'export EDITOR=vi' >> ~/.bashrc
 
 # make soft links
+sudo ln -sf /usr/local/bin/vim /usr/bin/vi
+sudo ln -sf /usr/local/bin/vim /usr/bin/vim
 ln -s ~/.ed-rc/.vimrc ~/.vimrc 
 ln -s  ~/.ed-rc/.tmux.conf ~/.tmux.conf
-
-# Install fzf 
-# TODO non-interactive install
-# TODO use fd as default finder
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf 
-~/.fzf/install
-
-#Install fd 
-wget https://github.com/sharkdp/fd/releases/download/v7.3.0/fd_7.3.0_amd64.deb -O /tmp/fd.deb
-sudo dpkg -i /tmp/fd.deb
-# respecting .gitignore
-echo 'export FZF_DEFAULT_COMMAND="fd --type f"' >> ~/.bashrc
 
 # vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# nodejs (required for coc.nvim)
-curl -sL install-node.now.sh/lts | sudo bash
-    # Optional install yarn if you want install extension by CocInstall command
-curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
+# Install fzf 
+# TODO non-interactive install
+# TODO use fd as default finder
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf 
+yes Y | ~/.fzf/install
+
+# respecting .gitignore
+echo 'export FZF_DEFAULT_COMMAND="fd --type file --follow --hidden --exclude .git --color=always"' >> ~/.bashrc
+echo 'export FZF_DEFAULT_OPTS="--ansi"' >> ~/.bashrc
+echo 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' >> ~/.bashrc
 
 #Install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# Oh my Bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+# nvm and node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# source current oh my bash
+source ~/.bashrc
+
+nvm install --lts
+vim + 'PlugInstall --sync' +qa 
+
 
